2f7fd464e590f04a5f90c2ddf018b415f0a60972
Marcus Schappi <marcus@schappi.com>
2023-05-02 08:47:26 +1000

    go to etsy stores

--- app/controllers/betsy/response_listener_controller.rb
+++ app/controllers/betsy/response_listener_controller.rb
@@ -1,5 +1,6 @@
 class Betsy::ResponseListenerController < ApplicationController
   def etsy_response_listener
     Betsy.request_access_token(params)
+    redirect_to '/admin/etsy_stores'
   end
 end
